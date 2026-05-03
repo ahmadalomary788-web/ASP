069926f8a39bdf2c75490576931e10afd0c86d6f
Ahmad Alomary <ahmadalomary788@gmail.com>
2026-05-04 00:02:20 +0300

    Initial commit - ASP.NET Project

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class UserController : Controller
+{
+    public IActionResult SignIn()
+    {
+        return View();
+    }
+
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+
+    public IActionResult Profile()
+    {
+        return View();
+    }
+}
