885b6fab7e996ef88c62c14335411ef107c8563f
Ahmad Alomary <ahmadalomary788@gmail.com>
2026-05-05 00:16:08 +0300

    update files

---
+++ WebApplication1/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace OYIL.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            var usersList = new List<string[]>
+            {
+                new string[] { "1", "Ahmad", "ahmad@email.com", "Active" },
+                new string[] { "2", "Omar", "omar@email.com", "Pending" },
+                new string[] { "3", "Ali", "ali@email.com", "Active" }
+            };
+
+            ViewBag.Users = usersList;
+
+            return View();
+        }
+    }
+}
