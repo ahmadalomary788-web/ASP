7bdb71ff328c61e75f345d1b962945d782e40725
Ahmad Alomary <ahmadalomary788@gmail.com>
2026-05-07 00:48:14 +0300

    update files

---
+++ Core MVC 4-5-2026/OYIL/OYIL/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OYIL.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
---
+++ Core MVC 4-5-2026/OYIL/OYIL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OYIL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
